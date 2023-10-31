481a0c6fe2b687fadb7f9cd453bca9870340c222
Austen Wade <austenwade@pop-os.localdomain>
2023-10-31 15:54:24 -0600

    some project updates

--- copy.cjs
+++ copy.cjs
@@ -1,16 +1,6 @@
-const { readFileSync, writeFileSync } = require("node:fs");
-const path = require("node:path");
-const cheerio = require("cheerio");
+const fs = require("fs");
 
-const head = readFileSync(path.join(__dirname, `public/head.html`));
-const $ = cheerio.load(head.toString());
-const headTags = [];
-$("link[href], script[src], meta").each(function (i, el) {
-  if ("href" in el.attribs) {
-    el.attribs["href"] = `public${el.attribs["href"]}`;
-  } else if ("src" in el.attribs) {
-    el.attribs["src"] = `public${el.attribs["src"]}`;
-  }
-  headTags.push(el.toString());
-});
-writeFileSync(__dirname + "/head.html", $.toString());
+const head = fs.readFileSync("./public/head.html", "utf8");
+
+const headWithPublic = head.replace(/(href|src)="(?!http)/g, '$1="/public');
+fs.writeFileSync("./head.html", headWithPublic);
