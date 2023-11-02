8617c03715c6c8c67b9b01dae2b423066df7f28a
Austen Wade <austenwade@pop-os.localdomain>
2023-11-02 02:19:26 -0600

    removed copy

--- copy.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-
-const head = fs.readFileSync("./public/head.html", "utf8");
-
-const headWithPublic = head.replace(/(href|src)="(?!http)/g, '$1="/public');
-fs.writeFileSync("./head.html", headWithPublic);
