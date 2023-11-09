d38b7f3564f182aafad2c5ad6001e54b4e432316
Connor Wade <connor@hoodoo.digital>
2023-11-09 12:17:53 -0700

    cli: adjusted build

--- public/Hero-KZ3QBZ42.js
+++
@@ -1,8 +0,0 @@
-import{g as l}from"./chunk-YPXNN4AR.js";import"./chunk-GQSCBKA6.js";var r=`<div class="hero flex flex-col justify-center items-center relative mb-8">
-  <div class="image-wrapper">\${img}</div>
-  <div class="hero-content text-center z-10 px-4 py-8 absolute w-full">
-    <h1 class="text-white text-4xl font-bold mb-4">\${title}</h1>
-  </div>
-</div>
-`;var m=e=>{let a=e.nextElementSibling,t=e.querySelector(":scope > picture");return t.lastElementChild.hasAttribute("loading")&&t.lastElementChild.getAttribute("loading")==="lazy"&&t.lastElementChild.setAttribute("loading","eager"),l([e,a,t],r,i=>({title:i[1].textContent,id:i[1].id,img:i[2].outerHTML}))};export{m as Hero};
-//# sourceMappingURL=Hero-KZ3QBZ42.js.map
